a6036443137e9d5d2631cc70e8afe597498cc4e8
kirby-b <kirby.brodie66@gmail.com>
2023-12-11 18:54:28 -0500

    Create word_count_2.py

---
+++ WordCounter/word_count_2.py
@@ -0,0 +1,11 @@
+def main(): 
+    word_string = input("Please input a sentence:")
+    word_count(word_string)
+
+def word_count(word_string: str):
+    #Counts the spaces and makes an assumes no foul play
+    word_string.strip(" ")
+    total_words = word_string.count(" ") + 1
+    print(str(total_words))
+
+main()
