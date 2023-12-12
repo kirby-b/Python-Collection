bacfc194cff4a06a78920f794c5d6d8b7dc312dd
kirby-b <kirby.brodie66@gmail.com>
2023-12-12 17:22:31 -0500

    Update word_count.py
    Now with while loop

--- WordCounter/word_count.py
+++ WordCounter/word_count.py
@@ -5,11 +5,15 @@ def main():
 def word_count(word_string: str):
     #Uses array/list to count words
     #Makes a list by splitting the words with spaces as delimiters
+    y = 0
+    x = 0
     word_string.strip(" ")
     countable = word_string.split(" ")
-    for x in range(len(countable)- 1):
+    while x < len(countable):
         if countable[x] == "" or countable[x].isspace():
             countable.pop(x)
+        else:
+            print(countable[x])
+            x +=1
     print(str(len(countable)))
-
 main()
