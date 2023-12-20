5dc0f3aa71d801e3e6c2da67df640fb426f71966
kirby-b <kirby.brodie66@gmail.com>
2023-12-20 14:05:56 -0500

    Update PizzaOrder.py

--- PizzaOrder/PizzaOrder.py
+++ PizzaOrder/PizzaOrder.py
@@ -27,3 +27,25 @@ class Pizzas:
     print("You will not be asked for the rest of the pizza information. This includes a name, code, and how many to order. The price will be automatically generated.")
     while (i <= int(pizzas) - 1):
         value = getValue()
+        order.append("Menu Price: $" + str(value))
+        uniqueCode = input("Please input a three letter/number menu code for pizza number " + str(i + 1) + ": ")
+        while(True):
+            if len(uniqueCode) != 3:
+                uniqueCode = input("Invalid code. Please input a 3 letter/number code: ")
+            else:
+                break
+        print("You entered: " + str(uniqueCode))
+        order.append("Menu Code: " + str(uniqueCode))
+        name = input("Please input th pizza name: ")
+        order.append("Menu Name: " + str(name))
+        stock = input("Please input the amount of the " + str(name) + " pizza you want in the virtual storage freezer: ")
+        while(True):
+            if stock.isnumeric() == False: 
+                stock = input("Please input a number, not symbols or letters: ")
+            else:
+                break
+        order.append("Total Pizza Count: " + str(stock))
+        total = int(stock) * int(value)
+        order.append("Total Inventory Price: $ " + str(total))
+        i += 1
+    toString(order, pizzas) 
