c412502c98d0bc4709bbae6a21170e0993ff07f1
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-01-19 11:35:30 -0500

    Update tax.py

--- Taxes/tax.py
+++ Taxes/tax.py
@@ -1,8 +1,19 @@
 def main(): 
+    money = input("What is your monthly income?\n")
+    state_percent = input("What is your states income tax percent?\n")
+    while(True):
+        if !(money.isnumeric()):
+            money = input("Invalid income. Please input a number and nothing else (Example: 100000) \n")
+        if !(state_percent.isnumeric()) or 1 > int(state_percent) > 100 :
+            state_percent = input("Invalid state tax. Please input a plain number between 1-100. (Example: 69) \n")
+        else:
+            break
+    fed = income_tax(money)
+    state = state_tax(money , state_percent)
     
-def incomeTax(money):
+def income_tax(money):
 
-def stateTax(money, state_percent):
+def state_tax(money, state_percent):
 
 def medicare(money):
 
