802aaefcd337f44ec50dae7f9f5a21546299b06b
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-04 16:13:58 -0500

    Update WebsiteNameSlice.py
    I will test it later. I think it should work though

--- WebNameSlice/WebsiteNameSlice.py
+++ WebNameSlice/WebsiteNameSlice.py
@@ -1,11 +1,19 @@
-slice = slice(8,-4)
-website1 = ("https://google.com")
-website2 = ("https://w3schools.com")
+import re
 
-print("This program takes a sample website like this:")
-print(website1)
-print(website2)
-print("And turns them into this")
-print(website1[slice])
-print(website2[slice])
-print("This is just an example of what the program does. It isnt done yet and I will finish it when I get the chance to again...or feel like it.")
+def main():
+  website1 = ("https://google.com")
+  website2 = ("https://w3schools.com")
+  
+  print("This program takes a sample website like this:")
+  print(website1)
+  print(website2)
+  print("And turns them into this")
+  print(extract_name(website1))
+  print(extract_name(website2))
+
+def extract_name(website):
+  start = website.find("://", beg=0, end=len(website))
+  end = ""
+  dot_what = website.search("\..+.*" , txt).span()
+  end = dot_what[-1]
+  return website.slice(start, end)
