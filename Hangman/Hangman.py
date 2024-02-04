32e7b5d2557361800e3837694281fcdfef0cb889
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-04 12:54:30 -0500

    Update Hangman.py

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -65,28 +65,79 @@ lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeo
 sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra red orange yellow green blue indigo violet white black 
 brown grey square triangle rectangle circle ellipse rhombus trapezoid pentagon hexagon septagon octogon cube pyramid cylinder apple orangle lemon 
 lime pear watermelon grape grapefruit anserry banana cantaloupe mango strawberry tomato carrot cucumber blueberry peaans raspberry lettuce celery""".split()
-print("My version of Hangman comes with colors, animals, and fruits/vegetables in the default list of words. " +
-    "But I have two more catagories I can add if you would like: ")
-print("Would you like to add less known animals to the list (y or n)")
-ans = input().lower()
-if ans == "y":
-    words = words + lesserKnownAnimals
-while ans != "y" and ans != "n":
-    print("ERROR. Please enter y or n")
+
+def main():
+    addExtras()
+    print("H A N G M A N")
+    #The number of misses letters
+    missedLs = ""
+    #The number of correct letters
+    correctLs = ""
+    sw = getRandomWord(words)
+    gameIsDone = False
+    
+    while True:
+        displayBoard(missedLs, correctLs, sw)
+        # Let the player enter a letter.
+        guess = getGuess(missedLs + correctLs)
+    
+        if guess in sw:
+            correctLs = correctLs + guess
+    
+            # anseck if the player has won.
+            foundAllLetters = True
+            for i in range(len(sw)):
+                if sw[i] not in correctLs:
+                    foundAllLetters = False
+                    break
+            if foundAllLetters:
+                print("Yes! The secret word was \"" + sw + "\"! Good Job, YOU WIN!!!")
+                gameIsDone = True
+        else:
+            missedLs = missedLs + guess
+    
+            # anseck to see if player has guessed to many times and has lost
+            if len(missedLs) == len(HANGMAN_PICS) - 1:
+                displayBoard(missedLs, correctLs, sw)
+                print("You have run out of guesses!\nAfter " + 
+                    str(len(missedLs)) + " missed guesses and " +
+                    str(len(correctLs)) + " correct guesses, the word was \"" +
+                    sw +"\". Better luck next time!")
+                gameIsDone = True
+    
+        # Ask the player if they want to play again (but only if game has ended)
+        if gameIsDone:
+            if playAgain():
+                missedLs = ""
+                correctLs = ""
+                gameIsDone = False
+                sw = getRandomWord(words)
+            else:
+                break
+def addExtras():   
+    print("My version of Hangman comes with colors, animals, and fruits/vegetables in the default list of words. " +
+        "But I have two more catagories I can add if you would like: ")
+    print("Would you like to add less known animals to the list (y or n)")
     ans = input().lower()
     if ans == "y":
         words = words + lesserKnownAnimals
-    #Adds lesser known animals
-print("Would you like to add programming language names to the list (y or n)")
-ans = input().lower()
-if ans == "y":
-    words = words + programNames
-while ans != "y" and ans != "n":
-    print("ERROR. Please enter y or n")
+    while ans != "y" and ans != "n":
+        print("ERROR. Please enter y or n")
+        ans = input().lower()
+        if ans == "y":
+            words = words + lesserKnownAnimals
+        #Adds lesser known animals
+    print("Would you like to add programming language names to the list (y or n)")
     ans = input().lower()
     if ans == "y":
         words = words + programNames
-    #Adds stuff for the fellow nerds
+    while ans != "y" and ans != "n":
+        print("ERROR. Please enter y or n")
+        ans = input().lower()
+        if ans == "y":
+            words = words + programNames
+        #Adds stuff for the fellow nerds
+
 def getRandomWord(wordList):
     # This function returns a random string from the passed list of of strings.
     wordIndex = random.randint(0,len(wordList) - 1)
@@ -134,50 +185,5 @@ def playAgain():
     print("Do you want to play again? (yes or no)")
     return input().lower().startswith("y")
 
-print("H A N G M A N")
-#The number of misses letters
-missedLs = ""
-#The number of correct letters
-correctLs = ""
-sw = getRandomWord(words)
-gameIsDone = False
-
-while True:
-    displayBoard(missedLs, correctLs, sw)
-    # Let the player enter a letter.
-    guess = getGuess(missedLs + correctLs)
-
-    if guess in sw:
-        correctLs = correctLs + guess
-
-        # anseck if the player has won.
-        foundAllLetters = True
-        for i in range(len(sw)):
-            if sw[i] not in correctLs:
-                foundAllLetters = False
-                break
-        if foundAllLetters:
-            print("Yes! The secret word was \"" + sw + "\"! Good Job, YOU WIN!!!")
-            gameIsDone = True
-    else:
-        missedLs = missedLs + guess
-
-        # anseck to see if player has guessed to many times and has lost
-        if len(missedLs) == len(HANGMAN_PICS) - 1:
-            displayBoard(missedLs, correctLs, sw)
-            print("You have run out of guesses!\nAfter " + 
-                str(len(missedLs)) + " missed guesses and " +
-                str(len(correctLs)) + " correct guesses, the word was \"" +
-                sw +"\". Better luck next time!")
-            gameIsDone = True
-
-    # Ask the player if they want to play again (but only if game has ended)
-    if gameIsDone:
-        if playAgain():
-            missedLs = ""
-            correctLs = ""
-            gameIsDone = False
-            sw = getRandomWord(words)
-        else:
-            break
-
+if __name__ == "__main__":
+    main()
