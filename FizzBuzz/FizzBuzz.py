dd9c90b81424c32b939e1a76236bd0a9946c5edb
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-04 12:49:16 -0500

    Update FizzBuzz.py

--- FizzBuzz/FizzBuzz.py
+++ FizzBuzz/FizzBuzz.py
@@ -1,4 +1,14 @@
 class FizzBuzz:
+    def main():
+        print("\nInput Number 3:")
+        fizzBuzz(3)
+        print("\nInput Number 5:")
+        fizzBuzz(5)
+        print("\nInput Number 10:")
+        fizzBuzz(10)
+        print("\nInput Number 20:")
+        fizzBuzz(20)
+    
     def fizzBuzz(n: int):
         for i in range(1, n+1):
             if i%5 == 0 and i%3 == 0: #If it is divisible by 5 and 3 it prints FizzBuzz
@@ -10,12 +20,6 @@ class FizzBuzz:
             elif i%5 == 0: #If it is divisible by 5 it prints Buzz
                 print("Buzz", end=" ")
             #Prints a phrase based on the current number being tested
-        
-    print("\nInput Number 3:")
-    fizzBuzz(3)
-    print("\nInput Number 5:")
-    fizzBuzz(5)
-    print("\nInput Number 10:")
-    fizzBuzz(10)
-    print("\nInput Number 20:")
-    fizzBuzz(20)
+    
+    if __name__ == "__main__":
+        main()
