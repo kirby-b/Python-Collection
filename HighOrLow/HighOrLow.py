3b574eaee43df8d7c7d7e9ebb3ea4fe832e8322c
kirby-b <kirby.brodie66@gmail.com>
2024-02-04 23:11:48 -0500

    Update HighOrLow.py

--- HighOrLow/HighOrLow.py
+++ HighOrLow/HighOrLow.py
@@ -1,46 +1,46 @@
 import random
 
-def highORlowfunc():
+def high_or_low_func():
     print("HIGHER or LOWER:")
     rando = random.randrange(1,11)
     print ("The number is: "+rando)
     #Generates a random number and prints it. 
     print("Will the next number be Higher or Lower.")
-    HorL = str.lower(str.strip(input()))
+    h_or_l = str.lower(str.strip(input()))
     #Holds the users answer
-    while HorL != "higher" and HorL != "lower" and HorL != "h" and HorL != "l":
+    while h_or_l != "higher" and h_or_l != "lower" and h_or_l != "h" and h_or_l != "l":
         print("Error, try again. Please input higher or lower.")
-        HorL = str.lower(str.strip(input()))
+        h_or_l = str.lower(str.strip(input()))
     #Asks if the user thinks the next number with be Higher or Lower
-    newrando = random.randrange(1,11)
+    new_rando = random.randrange(1,11)
     ans = ""
-    if rando > newrando:
-        print(newrando)
+    if rando > new_rando:
+        print(new_rando)
         print("Lower")
         ans = "lower"
-    if rando < newrando:
-        print(newrando)
+    if rando < new_rando:
+        print(new_rando)
         print("Higher")
         ans = "higher"     
     #Determines if the new number is Higher or Lower and tells the user
-    if ans == HorL:
+    if ans == h_or_l:
       print ("Correct")
     else :
         print ("Wrong, sorry")
     print ("Would you like to play again")
-    YesNo = str.lower(str.strip(input()))
+    yes_no = str.lower(str.strip(input()))
     #Tells user if they were right or now
-    while YesNo != "yes" and YesNo != "no":
+    while yes_no != "yes" and yes_no != "no":
         print("Error, try again")
-        YesNo = str.lower(str.strip(input()))
+        yes_no = str.lower(str.strip(input()))
 
-    if YesNo == "no":
+    if yes_no == "no":
         print("Goodbye!")
 
-    if YesNo == "yes":
+    if yes_no == "yes":
         print("Alright!")
-        highORlowfunc()
+        high_or_low_func()
     #Figure out if the user wants to play again
 
 if __name__ == "__main__":
-    highORlowfunc()
+    high_or_low_func()
