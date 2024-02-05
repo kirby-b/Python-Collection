d7627133170e08b2ab3c03d18f4fe5776824f918
kirby-b <kirby.brodie66@gmail.com>
2024-02-04 23:11:41 -0500

    Update DeductionGame.py

--- DeductionGame/DeductionGame.py
+++ DeductionGame/DeductionGame.py
@@ -12,47 +12,47 @@ def main():
     print(" Warm        One digit is correct and in the right position.")
     #Tells the user what the clues mean
     while True:
-        secretNum = getSecretNum()
+        secret_num = get_secret_num()
         print("I have generated a number. You have %s guesses to get it." % (MAX_GUESS))
     
-        guessesTaken = 1
-        while guessesTaken <= MAX_GUESS:
+        guesses_taken = 1
+        while guesses_taken <= MAX_GUESS:
             guess = ""
-            while len(guess) != NUM_DIGITS or not isOnlyDigits(guess):
-                print("Guess #%s: " % (guessesTaken))
+            while len(guess) != NUM_DIGITS or not is_only_digits(guess):
+                print("Guess #%s: " % (guesses_taken))
                 guess = input()
     
-            print(getClues(guess, secretNum))
-            guessesTaken += 1
+            print(get_clues(guess, secret_num))
+            guesses_taken += 1
     
-            if guess == secretNum:
+            if guess == secret_num:
                 break
-            if guessesTaken > MAX_GUESS:
-                print("You ran out of guesses. The answer was %s." % (secretNum))
+            if guesses_taken > MAX_GUESS:
+                print("You ran out of guesses. The answer was %s." % (secret_num))
         #Checks to see if the user guessed the number and if they have run out of guesses
         print("Do you want to play again? (yes or no)")
         if not input().lower().startswith("y"):
             break
         
-def getSecretNum():
+def get_secret_num():
     # Returns a string of unique random digits that is NUM_DIGITS long.
     numbers = list(range(10))
     random.shuffle(numbers)
-    secretNum = ""
+    secret_num = ""
     for i in range(NUM_DIGITS):
-        secretNum += str(numbers[i])
-    return secretNum
+        secret_num += str(numbers[i])
+    return secret_num
 
-def getClues(guess, secretNum):
+def get_clues(guess, secret_num):
     # Returns a string with the clues to the user.
-    if guess == secretNum:
+    if guess == secret_num:
         return "You got it!"
 
     clues = []
     for i in range(len(guess)):
-        if guess[i] == secretNum[i]:
+        if guess[i] == secret_num[i]:
             clues.append("Warm")
-        elif guess[i] in secretNum:
+        elif guess[i] in secret_num:
             clues.append("Cold")
     if len(clues) == 0:
         return "Nope"
@@ -60,7 +60,7 @@ def getClues(guess, secretNum):
     clues.sort()
     return " ".join(clues)
 
-def isOnlyDigits(num):
+def is_only_digits(num):
     # Checks to make sure the user only input digits. Returns true if they did, otherwise it is false.
     if num == "":
         return False
