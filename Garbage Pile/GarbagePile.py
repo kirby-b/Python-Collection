4e19c9e9fb9c478d6b89bade54f6df8d8f14d7c5
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-06 11:58:41 -0500

    Update GarbagePile.py

--- Garbage Pile/GarbagePile.py
+++ Garbage Pile/GarbagePile.py
@@ -1,4 +1,5 @@
 # This is a small collection of miscellaneous functions that I made for various purposes, only for me to find a fix that is already built in.
+# I put them here because I didnt feel they deserved a full repo, but could still be of use.
 
 # From my tax.py program. Replaced by an isinstance
 def is_float(string):
