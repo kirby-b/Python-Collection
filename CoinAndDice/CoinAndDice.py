094fa136f92664a795fd7781b05925737852ddd8
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-13 11:22:06 -0500

    Update CoinAndDice.py

--- CoinAndDice/CoinAndDice.py
+++ CoinAndDice/CoinAndDice.py
@@ -1,23 +1,14 @@
 import random
 
-def coin_n_dice():
+def main():
     print("Coin (two sides) or Dice (pick how many sides)")
     choice = str.lower(str.strip(input()))
     if choice == "dice":
-        choice = str.strip(input("How many sides do you want each die to have:"))
-        choice2 = str.strip(input("How many die would you like to roll:"))
-        print("\n")
-        for x in range(int(choice2)):
-            print(random.randrange(1,int(choice) + 1))
+        dice()
     #Rolls a die with the input number of sides, for the input number of dice
     if choice == "coin":
-        print("How many times would you like to flip")
-        choice = str.strip(input())
-        print("\n")
-        for x in range(int(choice)):
-            print(random.randrange(1,3))
+        coin_flip()
     #Flips a coin for the amount of times the user says to
-
     print ("Would you like to go again")
     YesNo = str.lower(str.strip(input()))
         
@@ -32,5 +23,22 @@ def coin_n_dice():
         print("Alright!")
         coin_n_dice()
     #Asking if the user want to go again
+
+def dice():
+    sides = str.strip(input("How many sides do you want each die to have:"))
+    amount = str.strip(input("How many die would you like to roll:"))
+    print("\n")
+    for x in range(int(amount)):
+        print(random.randrange(1,int(sides) + 1))
+#Rolls a die with the input number of sides, for the input number of dice
+
+def coin_flip():
+    print("How many times would you like to flip")
+    flips = str.strip(input())
+    print("\n")
+    for x in range(int(flips)):
+        print(random.randrange(1,3))
+#Flips a coin for the amount of times the user says to
+
 if __name__ == "__main__":
-    coin_n_dice()
+    main()
