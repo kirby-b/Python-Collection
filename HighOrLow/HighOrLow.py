82016eeb58037059d250658a25828238ae3ceaa6
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-02-13 11:39:41 -0500

    Update HighOrLow.py

--- HighOrLow/HighOrLow.py
+++ HighOrLow/HighOrLow.py
@@ -1,46 +1,40 @@
 import random
 
 def high_or_low_func():
-    print("HIGHER or LOWER:")
-    rando = random.randrange(1,11)
-    print ("The number is: "+rando)
-    #Generates a random number and prints it. 
-    print("Will the next number be Higher or Lower.")
-    h_or_l = str.lower(str.strip(input()))
-    #Holds the users answer
-    while h_or_l not in ["higher", "h", "lower", "l"]:
-        print("Error, try again. Please input higher or lower.")
-        h_or_l = str.lower(str.strip(input()))
-    #Asks if the user thinks the next number with be Higher or Lower
-    new_rando = random.randrange(1,11)
-    ans = ""
-    if rando > new_rando:
-        print(new_rando)
-        print("Lower")
-        ans = "lower"
-    if rando < new_rando:
-        print(new_rando)
-        print("Higher")
-        ans = "higher"     
-    #Determines if the new number is Higher or Lower and tells the user
-    if ans == h_or_l:
-      print ("Correct")
-    else :
-        print ("Wrong, sorry")
-    print ("Would you like to play again")
-    yes_no = str.lower(str.strip(input()))
-    #Tells user if they were right or now
-    while yes_no != "yes" and yes_no != "no":
-        print("Error, try again")
-        yes_no = str.lower(str.strip(input()))
-
-    if yes_no == "no":
-        print("Goodbye!")
-
-    if yes_no == "yes":
-        print("Alright!")
-        high_or_low_func()
-    #Figure out if the user wants to play again
+    while(True):
+        try:
+            print("HIGHER or LOWER:")
+            rando = random.randrange(1,11)
+            print ("The number is: "+rando)
+            #Generates a random number and prints it. 
+            print("Will the next number be Higher or Lower.")
+            h_or_l = str.lower(str.strip(input()))
+            #Holds the users answer
+            while h_or_l not in ["higher", "h", "lower", "l"]:
+                print("Error, try again. Please input higher or lower.")
+                h_or_l = str.lower(str.strip(input()))
+            #Asks if the user thinks the next number with be Higher or Lower
+            new_rando = random.randrange(1,11)
+            ans = ""
+            if rando > new_rando:
+                print(new_rando)
+                print("Lower")
+                ans = "lower"
+            if rando < new_rando:
+                print(new_rando)
+                print("Higher")
+                ans = "higher"     
+            #Determines if the new number is Higher or Lower and tells the user
+            if ans == h_or_l:
+              print ("Correct")
+            else :
+                print ("Wrong, sorry")
+            print ("Would you like to play again")
+            yes_no = str.lower(str.strip(input()))
+            #Tells user if they were right or now
+        except(KeyboardInterrupt):
+            print("Thank you for playing")
+            break
 
 if __name__ == "__main__":
     high_or_low_func()
