e1f4e5accfc9c680d4486bea199b1f8d627509a3
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 17:03:50 -0500

    Update Hangman.py

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -8,89 +8,95 @@ HANGMAN_PICS = ["""
         |
         |
         |
-       ===""","""
+       ===""", """
     +---+
     O   |
         |
         |
-       ===""","""
+       ===""", """
     +---+
     O   |
     |   |
         |
-       ===""",""""
+       ===""", """"
     +---+
     O   |
    /|   |
         |
-       ===""",""""
+       ===""", """"
     +---+
     O   |
-   /|\  |
+   /|\\  |
         |
-       ===""","""
+       ===""", """
     +---+
     O   |
-   /|\  |
+   /|\\  |
    /    |
-       ===""","""
+       ===""", """
     +---+
     O   |
-   /|\  |
-   / \  |
-       ===""","""
+   /|\\  |
+   / \\  |
+       ===""", """
     +---+
     O   |
-   /|\, |
-   / \  |
-       ===""","""
+   /|\\, |
+   / \\  |
+       ===""", """
     +---+
     O   |
-  ,/|\, |
-   / \  |
-       ===""","""
+  ,/|\\, |
+   / \\  |
+       ===""", """
     +---+
     O   |
-  ,/|\, |
-   / \. |
-       ===""","""
+  ,/|\\, |
+   / \\. |
+       ===""", """
     +---+
     O   |
-  ,/|\, |
-  ./ \. |
-       ==="""] # Hangman art
+  ,/|\\, |
+  ./ \\. |
+       ==="""]  # Hangman art
 
-#List of animals that are less common
-lesser_known_animals = "axolotl blobfish sawfish guitarfish komododragon serval kinkajou binturong capybara paca iguana mink".split()
-#List of programmming language names for nerds(I know some are debatable but deal with it.)
-program_names = """mysql java javascript bash docker python html c csharp cplusplus php rust assembly react swift cobol fortran go ruby perl laravel node git 
-lua shell typescipt kotlin fsharp assembly visualbasic gdscript matlab clojure css dotnet postgresql angular bootstrap kubernetes""".split()
-#Base words
-words = """ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle elephant ferret fox frog goat goose hawk
-lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk
-sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra red orange yellow green blue indigo violet white black 
-brown grey square triangle rectangle circle ellipse rhombus trapezoid pentagon hexagon septagon octogon cube pyramid cylinder apple orangle lemon 
-lime pear watermelon grape grapefruit anserry banana cantaloupe mango strawberry tomato carrot cucumber blueberry peaans raspberry lettuce celery""".split()
+# List of animals that are less common
+lesser_known_animals = """axolotl blobfish sawfish guitarfish komododragon serval kinkajou binturong capybara paca 
+iguana mink""".split()
+# List of programming language names for nerds(I know some are debatable but deal with it.)
+program_names = """mysql java javascript bash docker python html c csharp cplusplus php rust assembly react swift cobol 
+fortran go ruby perl laravel node git lua shell typescript kotlin fsharp assembly visualbasic gdscript matlab clojure 
+css dotnet postgresql angular bootstrap kubernetes""".split()
+# Base words
+words = """ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle elephant 
+ferret fox frog goat goose hawk lion lizard llama mole monkey moose mouse mule newt otter owl panda parrot pigeon python 
+rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle 
+weasel whale wolf wombat zebra red orange yellow green blue indigo violet white black brown grey square triangle 
+rectangle circle ellipse rhombus trapezoid pentagon hexagon heptagon octagon cube pyramid cylinder apple orange lemon 
+lime pear watermelon grape grapefruit banana cantaloupe mango strawberry tomato carrot cucumber blueberry peach 
+raspberry lettuce celery""".split()
+
 
 def main():
+    global words
     add_extras()
     print("H A N G M A N")
-    #The number of misses letters
+    # The number of misses letters
     missed_ls = ""
-    #The number of correct letters
+    # The number of correct letters
     correct_ls = ""
     sw = get_random_word(words)
     game_is_done = False
-    
+
     while True:
         display_board(missed_ls, correct_ls, sw)
         # Let the player enter a letter.
         guess = get_guess(missed_ls + correct_ls)
-    
+
         if guess in sw:
             correct_ls = correct_ls + guess
-    
-            # anseck if the player has won.
+
+            # Check if the player has won.
             found_all_letters = True
             for i in range(len(sw)):
                 if sw[i] not in correct_ls:
@@ -102,17 +108,17 @@ def main():
                 game_is_done = True
         else:
             missed_ls = missed_ls + guess
-    
-            # anseck to see if player has guessed to many times and has lost
+
+            # check to see if player has guessed to many times and has lost
             if len(missed_ls) == len(HANGMAN_PICS) - 1:
                 display_board(missed_ls, correct_ls, sw)
-                print("You have run out of guesses!\nAfter " + 
-                    str(len(missed_ls)) + " missed guesses and " +
-                    str(len(correct_ls)) + " correct guesses, the word was \"" +
-                    sw +"\". Better luck next time!")
+                print("You have run out of guesses!\nAfter " +
+                      str(len(missed_ls)) + " missed guesses and " +
+                      str(len(correct_ls)) + " correct guesses, the word was \"" +
+                      sw + "\". Better luck next time!")
                 keep_score("l")
                 game_is_done = True
-    
+
         # Ask the player if they want to play again (but only if game has ended)
         if game_is_done:
             if play_again():
@@ -122,20 +128,25 @@ def main():
                 sw = get_random_word(words)
             else:
                 break
-def add_extras():   
+
+
+def add_extras():
+    global words
+    global program_names
+    global lesser_known_animals
     print("My version of Hangman comes with colors, animals, and fruits/vegetables in the default list of words. " +
-        "But I have two more catagories I can add if you would like: ")
+          "But I have two more categories I can add if you would like: ")
     print("Would you like to add less known animals to the list (y or n)")
     ans = input().lower()
     # Gets input and formats it
     if ans == "y":
-        words = words + lesser_known_animals
+        words += lesser_known_animals
     while ans != "y" and ans != "n":
         print("ERROR. Please enter y or n")
         ans = input().lower()
         if ans == "y":
-            words = words + lesser_known_animals
-        #Adds lesser known animals
+            words += lesser_known_animals
+        # Adds lesser known animals
     print("Would you like to add programming language names to the list (y or n)")
     ans = input().lower()
     # Gets input and formats it
@@ -146,20 +157,22 @@ def add_extras():
         ans = input().lower()
         if ans == "y":
             words = words + program_names
-        #Adds stuff for the fellow nerds
+        # Adds stuff for the fellow nerds
+
 
 def get_random_word(word_list):
-    # This function returns a random string from the passed list of of strings.
-    word_index = random.randint(0,len(word_list) - 1)
+    # This function returns a random string from the passed list of strings.
+    word_index = random.randint(0, len(word_list) - 1)
     return word_list[word_index]
+
 
 def display_board(missed_ls, correct_ls, sw):
     print(HANGMAN_PICS[len(missed_ls)])
     print()
 
-    print("Missed letters:" , end = " ")
+    print("Missed letters:", end=" ")
     for letter in missed_ls:
-        print(letter, end= " ")
+        print(letter, end=" ")
     print()
 
     blanks = "_" * len(sw)
@@ -167,15 +180,17 @@ def display_board(missed_ls, correct_ls, sw):
     for i in range(len(sw)):
         # Replaces blanks with correctly guessed letters.
         if sw[i] in correct_ls:
-            blanks = blanks[:i] + sw[i] + blanks[i+1:]
-    
+            blanks = blanks[:i] + sw[i] + blanks[i + 1:]
+
     for letter in blanks:
-        # Show the secret word with spaces in between eaans letter.
+        # Show the secret word with spaces in between each letter.
         print(letter, end=" ")
     print()
 
+
 def get_guess(already_guessed):
-    # Returns the letter the player entered. This function makes sure the player entered a single letter and not something else
+    # Returns the letter the player entered. This function makes sure the player entered a single letter
+    # and not something else
     while True:
         print("Guess a letter.")
         guess = input()
@@ -184,16 +199,18 @@ def get_guess(already_guessed):
             print("Please enter a single letter.")
         elif guess in already_guessed:
             print("You already guessed that letter. Try again.")
-        #Makes sure they havent used the letter yet
+        # Makes sure they haven't used the letter yet
         elif guess not in "abcdefghijklmnopqrstuvwxyz":
             print("Please enter a LETTER.")
         else:
             return guess
 
+
 def play_again():
     # This function returns True if the player wants to play again; otherwise returns False.
     print("Do you want to play again? (yes or no)")
     return input().lower().startswith("y")
+
 
 def keep_score(winner):
     global loses
@@ -204,10 +221,11 @@ def keep_score(winner):
         loses += 1
     else:
         pass
-    #Calculates the new scores
-    
+    # Calculates the new scores
+
     print(f"Games Won:{player_score}\nGames Lost:{loses}")
-    #Prints current scores
+    # Prints current scores
+
 
 if __name__ == "__main__":
     main()
