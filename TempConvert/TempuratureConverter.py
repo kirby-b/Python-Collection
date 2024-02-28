91fb305d61703a9e1d96945eb586b61fcf18dd7a
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 17:04:28 -0500

    Update TempuratureConverter.py

--- TempConvert/TempuratureConverter.py
+++ TempConvert/TempuratureConverter.py
@@ -1,28 +1,31 @@
 def main():
     temp = float(input("Enter the temperature: "))
-    #Gets unit of temperature and its value. There was no reason to include kelvin, but I was bored.
-    unit = input("Is this tempurature in Celsius, Fahrenheit, Kelvin (C/F/K):").lower()
-    while unit not in ["celsius","c","fahrenheit","f","kelvin","k"]:
-        unit = input("Please input a valid tempurature unit (Celsius, Fahrenheit, Kelvin, C, F, K:").lower()
-    
+    # Gets unit of temperature and its value. There was no reason to include kelvin, but I was bored.
+    unit = input("Is this temperature in Celsius, Fahrenheit, Kelvin (C/F/K):").lower()
+    while unit not in ["celsius", "c", "fahrenheit", "f", "kelvin", "k"]:
+        unit = input("Please input a valid temperature unit (Celsius, Fahrenheit, Kelvin, C, F, K:").lower()
+
+
 def convert(unit, temp):
     if unit == "c":
         ntemp = round((9 * temp) / 5 + 32, 2)
         print(f"The temperature in Fahrenheit is: {ntemp}°F")
-        ntemp = round(temp + 273.15 , 2)
+        ntemp = round(temp + 273.15, 2)
         print(f"The temperature in Kelvin is: {ntemp}°K")
-    #Prints what a temp in Celsius is in Kelvin and Fahrenheit
+    # Prints what a temp in Celsius is in Kelvin and Fahrenheit
     elif unit == "f":
         ntemp = round((temp - 32) * 5 / 9, 2)
         print(f"The temperature in Celsius is: {ntemp}°C")
-        ntemp = round((temp - 32) * 5/9 + 273.15, 2)
+        ntemp = round((temp - 32) * 5 / 9 + 273.15, 2)
         print(f"The temperature in Kelvin is: {ntemp}°K")
-        #Prints what a temp in Fahrenheit is in Celcius and kelvin
+        # Prints what a temp in Fahrenheit is in Celsius and kelvin
     elif unit == "k":
-        ntemp = round(temp - 273.15 , 2)
+        ntemp = round(temp - 273.15, 2)
         print(f"The temperature in Celsius is: {ntemp}°C")
-        ntemp = round((temp - 273.15) * 9/5 + 32, 2)
+        ntemp = round((temp - 273.15) * 9 / 5 + 32, 2)
         print(f"The temperature in Fahrenheit is: {ntemp}°F")
-    #Prints what a temp in Kelvin is in Celsius and Fahrenheit
+    # Prints what a temp in Kelvin is in Celsius and Fahrenheit
+
+
 if __name__ == "__main__":
     main()
