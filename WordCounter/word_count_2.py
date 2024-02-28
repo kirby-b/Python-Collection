9e06b78f7fdaef3b53572e04b3406a12c0da1e22
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 17:04:39 -0500

    Update word_count_2.py

--- WordCounter/word_count_2.py
+++ WordCounter/word_count_2.py
@@ -1,13 +1,15 @@
-def main(): 
+def main():
     word_string = input("Please input a sentence:")
     word_count(word_string)
 
+
 def word_count(word_string: str):
-    #Counts the spaces and assumes no foul play
+    # Counts the spaces and assumes no foul play
     word_string.strip(" ")
     total_words = word_string.count(" ") + 1
-    #Assumes there is one space per word except the last
+    # Assumes there is one space per word except the last
     print(str(total_words))
+
 
 if __name__ == "__main__":
     main()
