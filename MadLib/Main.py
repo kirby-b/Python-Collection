14e4a634098c60bb752262af95d589defd862eb5
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 17:04:16 -0500

    Update Main.py

--- MadLib/Main.py
+++ MadLib/Main.py
@@ -5,14 +5,15 @@ print("""Welcome to my MadLib program. Would you like to play:
     A Crazy MadLib
     (please type simple, crazy, c, or s)""")
 ans = str.lower(str.strip(input()))
-#Gets the mode answer from the user
+# Gets the mode answer from the user
 while ans != "simple" and ans != "s" and ans != "crazy" and ans != "c":
-        print("Error, try again")
-        ans = str.lower(str.strip(input()))
-    #Checks if input is valid
+    print("Error, try again")
+    ans = str.lower(str.strip(input()))
+    # Checks if input is valid
 
+play = MadLibs()
 if ans == "simple" or ans == "s":
-    MadLibs.simple_mad_lib_mode_func()
+    play.simple_mad_lib_mode_func()
 if ans == "crazy" or ans == "c":
-    MadLibs.crazy_mad_lib_mode_func()
-#Prints the Madlib based on type from a function in the MadLib class
+    play.crazy_mad_lib_mode_func()
+# Prints the Madlib based on type from a function in the MadLib class
