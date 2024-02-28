4ee223b861a9456f1f88d54f4d2e8c3b69437cab
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 17:03:47 -0500

    Update GarbagePile.py

--- Garbage Pile/GarbagePile.py
+++ Garbage Pile/GarbagePile.py
@@ -1,17 +1,19 @@
-# This is a small collection of miscellaneous functions that I made for various purposes, only for me to find a fix that is already built in.
-# I put them here because I didnt feel they deserved a full repo, but could still be of use.
+# This is a small collection of miscellaneous functions that I made for various purposes, only for me to find a fix that
+# is already built in.
+# I put them here because I didn't feel they deserved a full repo, but could still be of use.
 
 # From my tax.py program. Replaced by an isinstance
 def is_float(string):
-   try:
-      float(string)#Turn the input into a float. If it is not a number or float, it will throw an exception
-      return True
-   except ValueError: #On exception it will return false
-      return False
+    try:
+        float(string)  # Turn the input into a float. If it is not a number or float, it will throw an exception
+        return True
+    except ValueError:  # On exception, it will return false
+        return False
 
-# From a lot of my projects. Replaced by a while loop with a try catch, the catch watches for a KeyboardInterupt.
+
+# From a lot of my projects. Replaced by a while loop with a try catch, the catch watches for a KeyboardInterrupt.
 while True:
-   #Code goes here
-   print("Do you want to play again? (yes or no)") #Happens when the game or whatever is done
-   if not input().lower().startswith("y"):
-      break
+    # Code goes here
+    print("Do you want to play again? (yes or no)")  # Happens when the game or whatever is done
+    if not input().lower().startswith("y"):
+        break
