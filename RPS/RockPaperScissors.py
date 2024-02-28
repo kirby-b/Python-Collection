4adb112e3e34ace061e51498ca3c237cf673bdf7
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 17:04:21 -0500

    Update RockPaperScissors.py

--- RPS/RockPaperScissors.py
+++ RPS/RockPaperScissors.py
@@ -1,56 +1,60 @@
 import random
-#Global Variables for scores
+
+# Global Variables for scores
 player_score = 0
 computer_score = 0
 times_cheated = 0
 ties = 0
 
+
 def rock_paper_scissor_func():
-    while(True):
+    while True:
         try:
             print("Rock \nPaper \nScissors \nShoot\n")
-        
-            RPS = random.randrange(1,4)
-            player_RPS = str.lower(str.strip(input()))
-            while player_RPS not in["rock","paper","scissors","gun"]:
+
+            rps = random.randrange(1, 4)
+            player_rps = str.lower(str.strip(input()))
+            while player_rps not in ["rock", "paper", "scissors", "gun"]:
                 print("Error, try again")
-                player_RPS = str.lower(str.strip(input()))
-            #Validates input
-        
-            if RPS == 1:
+                player_rps = str.lower(str.strip(input()))
+            # Validates input
+
+            if rps == 1:
                 print("Rock")
-            if RPS == 2:
+            if rps == 2:
                 print("Paper")
-            if RPS == 3:
+            if rps == 3:
                 print("Scissors")
-            #Determines the computers choice
-        
-            if player_RPS == "rock" and RPS == 3 or player_RPS == "scissors" and RPS == 2 or player_RPS == "paper" and RPS == 1:
+            # Determines the computers choice
+
+            if player_rps == "rock" and rps == 3 or player_rps == "scissors" and rps == 2 or player_rps == "paper" and rps == 1:
                 print("You Win")
                 keep_score("p")
-            elif player_RPS == "paper" and RPS == 3 or player_RPS == "rock" and RPS == 2 or player_RPS == "scissors" and RPS == 1:
+            elif player_rps == "paper" and rps == 3 or player_rps == "rock" and rps == 2 or player_rps == "scissors" and rps == 1:
                 print("You Lose")
                 keep_score("c")
-            elif player_RPS == "scissors" and RPS == 3 or player_RPS == "paper" and RPS == 2 or player_RPS == "rock" and RPS == 1:
+            elif player_rps == "scissors" and rps == 3 or player_rps == "paper" and rps == 2 or player_rps == "rock" and rps == 1:
                 print("Tie")
                 keep_score("t")
-            elif player_RPS == "gun":
+            elif player_rps == "gun":
                 print("Cheater")
                 keep_score("cheat")
-            #Determines who wins and calls the function to update the score
-        except(KeyboardInterrupt):
+            # Determines who wins and calls the function to update the score
+        except KeyboardInterrupt:
             print("Thank you for playing. Here are the final scores:")
             keep_score("thisstringdoesntmatter")
             break
-        #If the user uses a keyboard interupt, it will end the program with this catch
+        # If the user uses a keyboard interrupt, it will end the program with this catch
+
+
 def keep_score(winner):
     global times_cheated
-    global player_score 
+    global player_score
     global computer_score
-    global ties 
+    global ties
     if winner == "p":
         player_score += 1
-    elif winner == "c": 
+    elif winner == "c":
         computer_score += 1
     elif winner == "t":
         ties += 1
@@ -59,10 +63,14 @@ def keep_score(winner):
         times_cheated += 1
     else:
         pass
-    #Calculates the new scores
-    
-    print(f"Player Wins:{player_score}\nComputer Wins:{computer_score}\nTimes you tied:{ties}\nTimes you cheated:{times_cheated}")
-    #Prints current scores
+    # Calculates the new scores
+
+    print(f"""Player Wins:{player_score}
+    \nComputer Wins:{computer_score}
+    \nTimes you tied:{ties}
+    \nTimes you cheated:{times_cheated}""")
+    # Prints current scores
+
 
 if __name__ == "__main__":
     rock_paper_scissor_func()
