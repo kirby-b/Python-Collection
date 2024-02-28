3b3e6f6d63e7e004164d9895f9b7c07c0286ecf9
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 17:03:53 -0500

    Update HighOrLow.py

--- HighOrLow/HighOrLow.py
+++ HighOrLow/HighOrLow.py
@@ -1,21 +1,22 @@
 import random
 
+
 def high_or_low_func():
     print("Welcome to higher or lower. To stop the program please please control + C.")
-    while(True):
+    while True:
         try:
             print("HIGHER or LOWER:")
-            rando = random.randrange(1,11)
-            print ("The number is: "+ str(rando))
-            #Generates a random number and prints it. 
+            rando = random.randrange(1, 11)
+            print("The number is: " + str(rando))
+            # Generates a random number and prints it.
             print("Will the next number be Higher or Lower.")
             h_or_l = str.lower(str.strip(input()))
-            #Holds the users answer
+            # Holds the users answer
             while h_or_l not in ["higher", "lower"]:
                 print("Error, try again. Please input higher or lower.")
                 h_or_l = str.lower(str.strip(input()))
-            #Asks if the user thinks the next number with be Higher or Lower
-            new_rando = random.randrange(1,11)
+            # Asks if the user thinks the next number with be Higher or Lower
+            new_rando = random.randrange(1, 11)
             ans = ""
             if rando > new_rando:
                 print(new_rando)
@@ -24,16 +25,17 @@ def high_or_low_func():
             if rando < new_rando:
                 print(new_rando)
                 print("Higher")
-                ans = "higher"     
-            #Determines if the new number is Higher or Lower and tells the user
+                ans = "higher"
+                # Determines if the new number is Higher or Lower and tells the user
             if ans == h_or_l:
-              print ("Correct")
-            else :
-                print ("Wrong, sorry")
-            #Tells user if they were right or not
-        except(KeyboardInterrupt):
+                print("Correct")
+            else:
+                print("Wrong, sorry")
+            # Tells user if they were right or not
+        except KeyboardInterrupt:
             print("Thank you for playing")
             break
+
 
 if __name__ == "__main__":
     high_or_low_func()
