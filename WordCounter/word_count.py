495d75fec62d49001e8b50a164d1665d6d669c43
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 18:30:13 -0500

    Update word_count.py

--- WordCounter/word_count.py
+++ WordCounter/word_count.py
@@ -6,7 +6,6 @@ def main():
 def word_count(word_string: str):
     # Uses array/list to count words
     # Makes a list by splitting the words with spaces as delimiters
-    y = 0
     x = 0
     countable = word_string.split(" ")
     while x < len(countable):
