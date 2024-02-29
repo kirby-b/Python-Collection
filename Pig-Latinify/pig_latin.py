955743d6e3bc778b49cd53cd5d0025660f5a275c
kirby-b <kirby.brodie66@gmail.com>
2024-02-28 19:02:04 -0500

    Got bored and made something pointless
    - I guess its an example of string manipulation
    - Its just pig latin

---
+++ Pig-Latinify/pig_latin.py
@@ -0,0 +1,35 @@
+def main():
+    print("Pig Latin is defined as:"
+          + "\nA secret language formed from English by transferring the initial consonant or consonant cluster of "
+          + "each word to the end of the word and adding a vocalic syllable")
+    word_string = input("Please input a sentence:")
+    pigify(word_string)
+
+
+def pigify(word_string: str):
+    # Beginning of reused code from word count
+    # Uses array/list to count words
+    # Makes a list by splitting the words with spaces as delimiters
+    x = 0
+    countable = word_string.split(" ")
+    while x < len(countable):
+        if countable[x] == "" or countable[x].isspace():
+            countable.pop(x)  # Pops the index if it is empty or a space
+        else:
+            x += 1
+    # End of reused code
+    print("This is your sentence in pig latin")
+    for x in range(len(countable)):  # Loops through the list
+        word = countable[x]  # Gets the word
+        begin = word[0]  # Gets the beginning of the word
+        if begin in "aeiouAEIOU":
+            word = word + " tay"  # If it starts with a vowel, It just gets "tay" added to the end
+        else:
+            # If it begins with anything else(a consonant), It removes the first letter and adds it
+            # at the end with "ay"
+            word = word[1:] + " " + begin + "ay"
+        print(word, end=" ")
+
+
+if __name__ == "__main__":
+    main()
