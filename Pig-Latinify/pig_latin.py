8e48df0eb6e153b98678ef0c1657d63635e913aa
Brodie E Kirby <118732723+kirby-b@users.noreply.github.com>
2024-06-16 18:36:55 -0400

    more fixes

--- Pig-Latinify/pig_latin.py
+++ Pig-Latinify/pig_latin.py
@@ -2,9 +2,9 @@ def main():
     print("Pig Latin is defined as:"
           + "\nA secret language formed from English by transferring the initial consonant or consonant cluster of "
           + "each word to the end of the word and adding a vocalic syllable")
-    word_string = input("Please input a sentence:")
+    word_string = input("Please input a sentence: ")
     print("This is your sentence in pig latin")
-    print(pigify(word_string), end="")
+    print(pigify(word_string))
 
 
 def pigify(word_string: str):
@@ -12,6 +12,7 @@ def pigify(word_string: str):
     # Uses array/list to count words
     # Makes a list by splitting the words with spaces as delimiters
     x = 0
+    sentence = ""
     countable = word_string.split(" ")
     while x < len(countable):
         if countable[x] == "" or countable[x].isspace():
@@ -23,12 +24,13 @@ def pigify(word_string: str):
         word = countable[x]  # Gets the word
         begin = word[0]  # Gets the beginning of the word
         if begin in "aeiouAEIOU":
-            word = word + " yay"  # If it starts with a vowel, It just gets "yay" added to the end
+            word = word + " yay "  # If it starts with a vowel, It just gets "yay" added to the end
         else:
             # If it begins with anything else(a consonant), It removes the first letter and adds it
             # at the end with "ay"
-            word = word[1:] + " " + begin + "ay"
-        return word
+            word = word[1:] + " " + begin + "ay "
+        sentence += word 
+    return sentence.strip().lower()
 
 
 if __name__ == "__main__":
