0b01e1b5c7056046fbb70212c45fdd3ab2c67674
kirby-b <kirby.brodie66@gmail.com>
2024-09-13 17:38:18 -0400

    Added a while loop for input
    this makes it so you can only put whole numbers

--- TerminalBased/IntToBinary/int_to_bin.py
+++ TerminalBased/IntToBinary/int_to_bin.py
@@ -1,6 +1,11 @@
 binary_num = ""
-number = input("Please input a number (EX:2000): ")
-
+while True:
+    try:
+        number = input("Please input a whole number (EX:2000): ")
+        val = int(str(number))
+        break
+    except ValueError:
+        print("\nThat's not an whole number, try again.\n")
 while int(number) > 0:
     binary_num += str(int(number) % 2)
     number = int(number) / 2
