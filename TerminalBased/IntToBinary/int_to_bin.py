6ee8a76cf5f05c830c5a511f9221c3bfc8393cc5
kirby-b <kirby.brodie66@gmail.com>
2024-09-13 19:07:13 -0400

    Update int_to_bin.py

--- TerminalBased/IntToBinary/int_to_bin.py
+++ TerminalBased/IntToBinary/int_to_bin.py
@@ -1,6 +1,7 @@
 binary_num = ""
 while True:
     try:
+        # Gets whole number
         number = input("Please input a whole number (EX:2000): ")
         val = int(str(number))
         break
@@ -9,5 +10,7 @@ while True:
 while int(number) > 0:
     binary_num += str(int(number) % 2)
     number = int(number) / 2
-
+# The while loop outputs a str of 0s and 1s from the remainder of current num after modulus.
+# The output itself will be the reverse of the binary for the input int. 
+# Because of this I output it "backwards" that way it is correct
 print(binary_num[::-1])
